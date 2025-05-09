b0231da0cb6cbbbe50a817bb200b0d5de8611005
Rmadillo <Rmadillo@users.noreply.github.com>
2025-05-09 16:40:59 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const daysEl = document.getElementById("days");
-const hoursEl = document.getElementById("hours");
-const minsEl = document.getElementById("mins");
-const secondsEl = document.getElementById("seconds");
 
-const newYears = "1 Oct 2025";
+
+const newYears = "29 Nov 2025";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
@@ -12,14 +10,8 @@ function countdown() {
     const totalSeconds = (newYearsDate - currentDate) / 1000;
 
     const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const mins = Math.floor(totalSeconds / 60) % 60;
-    const seconds = Math.floor(totalSeconds) % 60;
 
     daysEl.innerHTML = days;
-    hoursEl.innerHTML = formatTime(hours);
-    minsEl.innerHTML = formatTime(mins);
-    secondsEl.innerHTML = formatTime(seconds);
 }
 
 function formatTime(time) {
